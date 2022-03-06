5c78b22af14bedc58bb72d9856d81f3ef26b27ae
takahashi kazunari <kamenaris@gmail.com>
2022-03-06 13:53:43 +0900

    コンソールプログラムを作成、ビルド

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,24 @@ for (var i = 2; i < process.argv.length; i++) {
     data.push(Number(process.argv[i]))
 }
 console.log(data)
+console.log(`parameters: ${data}`)
+
+const f = aggergate()
 
 for (let item of data) {
-    const res = primeFactor(item)
-    console.log(item + '= ' + res)
+  const res = f(item)
+  console.log(res)
 }
 
-function primeFactor(a: number): number[] {
-    const v: number[] = []
-    let x = a
-    let n = 2
-    while (x > n) {
-        if (x % n == 0) {
-            x = x / n
-            v.push(n)
-        } else {
-            n += n == 2 ? 1 : 2
-        }
-    }
-    v.push(x)
-    return v
+function aggergate(): (n:number) => [number, number, number, number, number] {
+  let total = 0
+  let totalp = 0
+  let totalt = 0
+  return (n:number):[number, number, number, number, number] => {
+    total += n
+    let tax = Math.floor(n - n / 1.1)
+    totalp += n - tax
+    totalt += tax
+    return [n, tax, total, totalp, totalt]
+  }
 }
